2c541b69f95037ab314d26b51f97346e4b9ee873
Danyella <danyellastrikann@duck.com>
2024-12-16 15:54:14 +0100

    Init

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
 ];
